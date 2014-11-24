a5fba09c8944d6abe83fdedbf2d43661b5dd870f
dstammb13 <dominik.stammbach@uzh.ch>
2014-11-24 12:35:15 +0100

    test

---
+++ test/com/test/RPC/testTest.java
@@ -0,0 +1,15 @@
+package com.test.RPC;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		//to be inserted
+	}
+
+}
